db28adbf1098b6a90f2527892b705f19d009eccd
lars <lorenz91@t-online.de>
2017-01-03 18:07:33 +0100

    Implement Client

--- PongSpezial/src/pongSpezial/netController/Client.java
+++ PongSpezial/src/pongSpezial/netController/Client.java
@@ -1,5 +1,41 @@
 package pongSpezial.netController;
 
-public class Client {
+import pongSpezial.dataModel.BoardState;
 
+public class Client implements Runnable
+{
+
+	private BoardState boardState;
+	private NetworkAddress serverIP;
+	
+	public Client(BoardState boardState, NetworkAddress serverIP)
+	{
+		this.boardState = boardState;
+		this.serverIP = serverIP;
+	}
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			validateInput();
+			System.out.println("Input");
+			Thread.sleep(10);
+		} catch (Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	public void validateInput()
+	{
+		
+	}
+	
+	public void updateGUI()
+	{
+		
+	}
+	
 }
