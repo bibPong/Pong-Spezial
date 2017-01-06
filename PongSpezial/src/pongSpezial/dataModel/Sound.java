bb79dd8bdb54888aa69e2c8972952fc877386854
pbs2h15asp <romanspeer@web.de>
2017-01-06 13:45:07 +0100

    Sound
    Sound abspielen und stoppen. Überlappen möglich

--- PongSpezial/src/pongSpezial/dataModel/Sound.java
+++ PongSpezial/src/pongSpezial/dataModel/Sound.java
@@ -1,13 +1,18 @@
 package pongSpezial.dataModel;
 
 import java.io.File;
-import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.Media;
 
 public class Sound 
 {
-	private Dictionary<String, Media> sounds;
+	private HashMap<String, Media> sounds;										//HashMap with the name and the sound
+	private Vector<MediaPlayer> mediaPlayers = new Vector<MediaPlayer>();		// Vector with MediaPlayers
 
 	public void sound()
 	{
@@ -16,14 +21,29 @@ public class Sound
 
 	public void startSound(String sound)
 	{
+		for (int i = 0; i < mediaPlayers.size(); i++)							// search sound in the vector mediaPlayers and start it
+		{
+			if(mediaPlayers.elementAt(i).equals(sound))
+			{
+				mediaPlayers.elementAt(i).play();
+			}
+		}		
 	}
 
 	public void stopSound(String sound)
 	{
+		for (int i = 0; i < mediaPlayers.size(); i++)							// search sound in the vector mediaPlayers and stop it
+		{
+			if(mediaPlayers.elementAt(i).equals(sound))
+			{
+				mediaPlayers.elementAt(i).stop();
+			}
+		}
 	}
 
 	private void init()
 	{
+		//Create the Files
 		Media background = new Media(new File("background.mp3").toURI().toString());
 		Media barCollision = new Media(new File("barCollision.mp3").toURI().toString());
 		Media powerUp = new Media(new File("powerUp.mp3").toURI().toString());
@@ -32,6 +52,7 @@ public class Sound
 		Media win = new Media(new File("win.mp3").toURI().toString());
 		Media lose = new Media(new File("lose.mp3").toURI().toString());
 
+		//Add Files
 		sounds.put("background", background);
 		sounds.put("barCollision", barCollision);
 		sounds.put("powerUp", powerUp);
@@ -39,5 +60,11 @@ public class Sound
 		sounds.put("countdown", countdown);
 		sounds.put("win", win);
 		sounds.put("lose", lose);
+		
+		//Add MediaPlayer
+		for (String sound: sounds.keySet())
+		{
+			mediaPlayers.add(new MediaPlayer(sounds.get(sound)));
+		}
 	}
 }
