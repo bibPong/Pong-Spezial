cc2632c4e2574587244082099d235ff78c464d26
pbs2h15alo <pbs2h15alo@pb.bib.de>
2017-01-06 08:12:40 +0100

    Test coomit

--- PongSpezial/src/pongSpezial/dataModel/Player.java
+++ PongSpezial/src/pongSpezial/dataModel/Player.java
@@ -3,9 +3,6 @@ package pongSpezial.dataModel;
 public class Player {
 	// Hier wurde etwas geändert
 	
-<<<<<<< HEAD
 	int conflict = 10;
-=======
-	int conflict = 7;
->>>>>>> branch 'master' of https://github.com/fortytwoish/Pong-Spezial.git
+
 }
