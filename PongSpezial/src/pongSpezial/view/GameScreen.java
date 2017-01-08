a49e0d56143a6fc74bcd46f752435faeee1c7e75
Admin <Admin@Admin-PC.Speedport_W_921V_1_42_000>
2017-01-08 20:06:01 +0100

    Controller in GameScreen.fxml wieder hinzugefügt  Methoden für die Buttons auch wieder hinzugefügt

--- PongSpezial/src/pongSpezial/view/GameScreen.java
+++ PongSpezial/src/pongSpezial/view/GameScreen.java
@@ -1,5 +1,34 @@
 package pongSpezial.view;
 
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+
 public class GameScreen {
 
+	@FXML
+	private Button closeGamescreen;
+	@FXML
+	private ToggleButton soundToggle;
+	
+	
+	@FXML
+	private void switchOnOff()
+	{
+		
+		
+	}
+	
+	@FXML
+	private void closeScreen()
+	{
+		
+		
+		   Stage stage = (Stage) closeGamescreen.getScene().getWindow();
+		    stage.close();
+	}
+	
 }
